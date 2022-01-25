19a48aa9ce919874cface5e43185ed6e95bf33ff
jsdevraju <islamrajulearn.dev@gmail.com>
2022-01-25 18:39:48 +0600

    my project build done

--- js/app.js
+++ js/app.js
@@ -3,7 +3,13 @@ const navbar =  document.querySelector('.navbar');
 const search  = document.querySelector('.fa-search');
 const overlay = document.querySelector('.overlay');
 const searchFeild =  document.querySelector('.search_box');
+const testimonial_main =  document.querySelectorAll('.testimonial_main');
+const prev =  document.querySelector('.fa-chevron-left');
+const next =  document.querySelector('.fa-chevron-right');
 
+
+//track current data 
+let current = 0;
 
 window.addEventListener('scroll', () =>{
     navbar.classList.toggle('bgcolor', window.scrollY > 0);
@@ -18,4 +24,47 @@ search.addEventListener('click', () =>{
 overlay.addEventListener('click', () =>{
     overlay.classList.remove('show');
     searchFeild.classList.remove('show')
+})
+
+const changeSlider = () =>{
+    testimonial_main.forEach((slide, index) =>{
+        if(slide.classList.contains('active')) current = index
+    })
+    testimonial_main[current].classList.remove('active');
+    if(current === (testimonial_main.length -1)) testimonial_main[0].classList.add('active');
+    else testimonial_main[current + 1].classList.add('active')
+}
+
+
+let interval = setInterval(changeSlider, 3000)
+
+prev.addEventListener('click', () =>{
+    clearInterval(interval)
+    testimonial_main[current].classList.remove('active')
+
+    if(current === 0) {
+        testimonial_main[testimonial_main.length - 1].classList.add('active')
+        current = testimonial_main.length - 1
+    }
+    else {
+        testimonial_main[current - 1].classList.add('active')
+        current = current - 1
+    }
+    interval = setInterval(changeSlider, 5000)
+})
+
+
+next.addEventListener('click', () =>{
+    clearInterval(interval)
+    testimonial_main[current].classList.remove('active')
+
+    if(current === (testimonial_main.length - 1)) {
+        testimonial_main[0].classList.add('active')
+        current = 0
+    }
+    else {
+        testimonial_main[current + 1].classList.add('active')
+        current = current + 1
+    }
+    interval = setInterval(changeSlider, 5000)
 })
